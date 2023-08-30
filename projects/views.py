2ad1dbdfa06caa1a0772f9dc0a0279a94a8c1b3d
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-08-30 17:54:08 -0300

    4 - Customize as ViewSets para Profile

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.shortcuts import render
 from projects.models import Profile, Project
 from .serializers import ProfileSerializer, ProjectSerializer
 
@@ -8,6 +10,25 @@ from .serializers import ProfileSerializer, ProjectSerializer
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == "GET":
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
+
+            return render(
+                request,
+                "projects/project_detail.html",
+                {
+                    "project": serializer.data,
+                },
+            )
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
